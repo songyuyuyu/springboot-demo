4bbb3fd89f765b04feee6aa3d34c8be292e2b56e
song_yu <song_yu@trip.com>
2021-08-20 14:33:00 +0800

    jjjjjj

--- src/test/java/com/song_yu/HelloControlerTest.java
+++ src/test/java/com/song_yu/HelloControlerTest.java
@@ -1,6 +1,7 @@
 package com.song_yu;
 
 import com.song_yu.controller.HelloController;
+import com.song_yu.controller.TestController;
 import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
  * @author:
  * @date:
  */
-public class HelloControlerTest {
+public class HelloControlerTest extends TestApplication {
     @Autowired
     HelloController helloController;
 
