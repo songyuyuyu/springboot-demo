eb1238c3b06c48d5dce12a31ab29e74d2dfba241
song_yu <song_yu@trip.com>
2021-08-20 15:29:17 +0800

    ddddddddddddd

--- src/test/java/com/song_yu/TestApplication.java
+++ src/test/java/com/song_yu/TestApplication.java
@@ -1,8 +1,11 @@
 package com.song_yu;
 
+import com.song_yu.controller.HelloController;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -13,6 +16,14 @@ import org.springframework.test.context.web.WebAppConfiguration;
 //由于是Web项目，Junit需要模拟ServletContext，因此我们需要给我们的测试类加上@WebAppConfiguration。
 @WebAppConfiguration
 public class TestApplication {
+
+    @Autowired
+    HelloController helloController;
+
+    @Test
+    public void test(){
+        helloController.hello();
+    }
 
     @Before
     public void init() {
